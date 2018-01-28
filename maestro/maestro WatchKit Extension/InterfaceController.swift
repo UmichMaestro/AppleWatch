2d757b61d1686310f894988cefd48c82d9aba41f
Jake Baur <cjbaur@umich.edu>
2018-01-28 16:05:22 -0500

    Added the watch app files

---
+++ maestro/maestro WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  maestro WatchKit Extension
+//
+//  Created by Christopher Baur on 1/28/18.
+//  Copyright © 2018 Maestro_MDP.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ maestro/maestro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  maestro
+//
+//  Created by Christopher Baur on 1/28/18.
+//  Copyright © 2018 Maestro_MDP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

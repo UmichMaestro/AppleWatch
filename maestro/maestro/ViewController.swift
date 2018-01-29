a1167cbfdf9195a5f2974c9e8495ead4b259b596
Jake Baur <cjbaur@umich.edu>
2018-01-29 02:38:00 -0500

    Added some simple buttons and labels to the iOS app

--- maestro/maestro/ViewController.swift
+++ maestro/maestro/ViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var paired_status: UILabel!
+    
+    @IBOutlet weak var pairing_button: UIButton!
+    var paired = false
+    
+    @IBAction func beginPairing(_ sender: Any) {
+        if paired {
+            paired_status.text = "Unpaired"
+            pairing_button.backgroundColor = UIColor.blue
+            pairing_button.setTitle("Begin Pairing", for: .normal)
+            paired = false
+        } else {
+            paired_status.text = "Paired"
+            pairing_button.backgroundColor = UIColor.red
+            pairing_button.setTitle("End Pairing", for: .normal)
+            paired = true
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.

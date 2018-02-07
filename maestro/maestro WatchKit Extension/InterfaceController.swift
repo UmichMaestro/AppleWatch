c3b1b5edd3b30aa98ae76683744b1d32c0f96fd4
Jake Baur <cjbaur@umich.edu>
2018-02-07 15:55:53 -0500

    trying different things for accessing data

--- maestro/maestro WatchKit Extension/InterfaceController.swift
+++ maestro/maestro WatchKit Extension/InterfaceController.swift
@@ -32,9 +32,9 @@ class InterfaceController: WKInterfaceController {
         self.xValues.setText("intial")
         self.yValues.setText("intial")
         self.zValues.setText("intial")
-        self.xValueGyro.setText("intial")
-        self.yValueGyro.setText("intial")
-        self.zValueGyro.setText("intial")
+        self.xValueGyro.setText("gyroIntial")
+        self.yValueGyro.setText("gyroIntial")
+        self.zValueGyro.setText("gyroInitial")
         
         print("gets to post-interface")
     }
@@ -55,17 +55,70 @@ class InterfaceController: WKInterfaceController {
     func getMotionManagerUpdates() {
         
         // operation main queue
-        let mainQueue: OperationQueue = OperationQueue.main
+        //let accelQueue: OperationQueue = OperationQueue.main
+        let gyroQueue: OperationQueue = OperationQueue.main
         
         // init interval for update (NSTimeInterval)
         self.motionManager.accelerometerUpdateInterval = 0.1
         self.motionManager.gyroUpdateInterval = 0.1
         
+        // get current gyro data
+        if (self.motionManager.isDeviceMotionAvailable) {
+            
+            let gyroHandler: CMDeviceMotionHandler = { (gyroData:CMDeviceMotion?, error:Error?) -> Void in
+                // errors
+                if (error != nil) {
+                    print("error: \(String(describing: error?.localizedDescription))")
+                }else{
+                    // success
+                    if ((gyroData) != nil) {
+                        
+                        /*
+                        // get gyroscopes values
+                        let gyroX:String = String(format: "%.2f", (gyroData?.rotationRate.x)!) as String
+                        let gyroY:String = String(format: "%.2f", (gyroData?.rotationRate.y)!) as String
+                        let gyroZ:String = String(format: "%.2f", (gyroData?.rotationRate.z)!) as String
+                        
+                        print("gyro x: \(gyroX)")
+                        print("gyro y: \(gyroY)")
+                        print("gyro z: \(gyroZ)")
+                        
+                        // set text labels
+                        self.xValueGyro.setText(gyroX)
+                        self.yValueGyro.setText(gyroY)
+                        self.zValueGyro.setText(gyroZ)
+ 
+                        */
+                        
+                        // get accelerations values
+                        let x:String = String(format: "%.2f", (gyroData?.userAcceleration.x)!) as String
+                        let y:String = String(format: "%.2f", (gyroData?.userAcceleration.y)!) as String
+                        let z:String = String(format: "%.2f", (gyroData?.userAcceleration.z)!) as String
+                        
+                        print("accel x: \(x)")
+                        print("accel y: \(y)")
+                        print("accel z: \(z)")
+                        
+                        // set text labels
+                        self.xValues.setText(x)
+                        self.yValues.setText(y)
+                        self.zValues.setText(z)
+                    }
+                }
+            }
+            self.motionManager.startDeviceMotionUpdates(to: gyroQueue, withHandler: gyroHandler)
+        } else {
+            self.xValueGyro.setText("this is")
+            self.yValueGyro.setText("not")
+            self.zValueGyro.setText("available")
+        }
+        
+        /*
         // get current accelerometerData
-        if (self.motionManager.isAccelerometerAvailable) {
+        if (self.motionManager.is) {
             
             // define handler
-            let accelHandler: CMAccelerometerHandler = { (accelerometerData:CMAccelerometerData?, error:Error?) -> Void in
+            let accelHandler: CMDeviceMotionHandler = { (accelerometerData:CMDeviceMotion?, error:Error?) -> Void in
                 // errors
                 if (error != nil) {
                     print("error: \(String(describing: error?.localizedDescription))")
@@ -74,13 +127,13 @@ class InterfaceController: WKInterfaceController {
                     if ((accelerometerData) != nil) {
                         
                         // get accelerations values
-                        let x:String = String(format: "%.2f", (accelerometerData?.acceleration.x)!) as String
-                        let y:String = String(format: "%.2f", (accelerometerData?.acceleration.y)!) as String
-                        let z:String = String(format: "%.2f", (accelerometerData?.acceleration.z)!) as String
+                        let x:String = String(format: "%.2f", (accelerometerData?.userAcceleration.x)!) as String
+                        let y:String = String(format: "%.2f", (accelerometerData?.userAcceleration.y)!) as String
+                        let z:String = String(format: "%.2f", (accelerometerData?.userAcceleration.z)!) as String
                         
-                        print("x: \(x)")
-                        print("y: \(y)")
-                        print("z: \(z)")
+                        print("accel x: \(x)")
+                        print("accel y: \(y)")
+                        print("accel z: \(z)")
                         
                         // set text labels
                         self.xValues.setText(x)
@@ -91,42 +144,10 @@ class InterfaceController: WKInterfaceController {
             }
             
             // start accelerometer updates
-            self.motionManager.startAccelerometerUpdates(to: mainQueue, withHandler: accelHandler)
+            self.motionManager.startDeviceMotionUpdates(to: accelQueue, withHandler: accelHandler)
         }
+        */
         
-        // get current gyro data
-        if (self.motionManager.isGyroAvailable) {
-            
-            self.xValueGyro.setText("in available")
-            self.yValueGyro.setText("in available")
-            self.zValueGyro.setText("in available")
-            
-            let gyroHandler: CMGyroHandler = { (gyroData:CMGyroData?, error:Error?) -> Void in
-                // errors
-                if (error != nil) {
-                    print("error: \(String(describing: error?.localizedDescription))")
-                }else{
-                    // success
-                    if ((gyroData) != nil) {
-                        
-                        // get gyroscopes values
-                        let x:String = String(format: "%.2f", (gyroData?.rotationRate.x)!) as String
-                        let y:String = String(format: "%.2f", (gyroData?.rotationRate.y)!) as String
-                        let z:String = String(format: "%.2f", (gyroData?.rotationRate.z)!) as String
-                        
-                        print("x: \(x)")
-                        print("y: \(y)")
-                        print("z: \(z)")
-                        
-                        // set text labels
-                        self.xValueGyro.setText(x)
-                        self.yValueGyro.setText(y)
-                        self.zValueGyro.setText(z)
-                    }
-                }
-            }
-            self.motionManager.startGyroUpdates(to: mainQueue, withHandler: gyroHandler)
-        }
     }
 
 }

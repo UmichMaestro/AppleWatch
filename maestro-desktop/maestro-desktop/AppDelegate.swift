fb3727abe70867e83064e2c485478211fa5a4120
Jake Baur <cjbaur@umich.edu>
2018-02-08 14:34:35 -0500

    added desktop app

---
+++ maestro-desktop/maestro-desktop/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  maestro-desktop
+//
+//  Created by Christopher Baur on 2/8/18.
+//  Copyright © 2018 Christopher Baur. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ maestro-desktop/maestro-desktop/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  maestro-desktop
+//
+//  Created by Christopher Baur on 2/8/18.
+//  Copyright © 2018 Christopher Baur. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    
+    @IBOutlet var connectedLabel: NSTextField!
+    @IBOutlet var inputValue: NSTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

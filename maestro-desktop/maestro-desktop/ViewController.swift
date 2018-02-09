6415d0d2c9c74b2e22e235a7e5e45123d8522b87
Jake Baur <cjbaur@umich.edu>
2018-02-09 16:13:07 -0500

    tried some work with CoreBluetooth, but it doesn't seem to work

--- maestro-desktop/maestro-desktop/ViewController.swift
+++ maestro-desktop/maestro-desktop/ViewController.swift
@@ -7,16 +7,21 @@
 //
 
 import Cocoa
+import CoreBluetooth
 
 class ViewController: NSViewController {
 
+    
+    let ourUUID = CFUUIDCreateFromString(nil, "000" as CFString)
+    let centralManager = CBCentralManager()
     
     @IBOutlet var connectedLabel: NSTextField!
     @IBOutlet var inputValue: NSTextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("before scan")
+        scanForWatch()
         // Do any additional setup after loading the view.
     }
 
@@ -26,6 +31,31 @@ class ViewController: NSViewController {
         }
     }
 
-
+    
+    func scanForWatch() {
+        print("in scan")
+        print("after centralManager")
+        if centralManager.state == .poweredOn {
+            centralManager.scanForPeripherals(withServices:nil, options:nil )
+        } else {
+            print("bluetooth not turned on")
+        }
+        print("after scan")
+    }
+    
+    func centralManager(_ central: CBCentralManager,
+                                 didDiscover peripheral: CBPeripheral,
+                                 advertisementData: [String : Any],
+                                 rssi RSSI: NSNumber) {
+        print("found a peripheral")
+        print("it's local name key is")
+        print(advertisementData[CBAdvertisementDataLocalNameKey]!)
+        if advertisementData[CBAdvertisementDataIsConnectable] != nil {
+            if (advertisementData[CBAdvertisementDataServiceUUIDsKey] as AnyObject).contains(ourUUID) {
+                let dataServiceArray = advertisementData[CBAdvertisementDataServiceDataKey]!
+                let ourData = (dataServiceArray as AnyObject)[ourUUID!]
+            }
+        }
+    }
 }
 

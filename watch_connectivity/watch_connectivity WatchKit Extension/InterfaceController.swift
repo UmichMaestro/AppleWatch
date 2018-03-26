c0d22f74c3493fa40e47616b12ec1bfbc70f1cfe
Jake Baur <cjbaur@umich.edu>
2018-03-26 18:59:53 -0400

    some watch connectivity code

--- watch_connectivity/watch_connectivity WatchKit Extension/InterfaceController.swift
+++ watch_connectivity/watch_connectivity WatchKit Extension/InterfaceController.swift
@@ -8,9 +8,25 @@
 
 import WatchKit
 import Foundation
+import WatchConnectivity
 
 
-class InterfaceController: WKInterfaceController {
+class InterfaceController: WKInterfaceController, WCSessionDelegate {
+    @IBOutlet var latency: WKInterfaceLabel!
+    
+    // not really sure what this does
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        return
+    }
+    
+    func session(_ session: WCSession, didReceiveMessageData messageData: Data) {
+        let time_at_receive = CFAbsoluteTimeGetCurrent()
+        let time_in = messageData.to(type: Double.self)
+        
+        let diff = time_at_receive - time_in
+        latency.setText(String(diff))
+    }
+    
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
@@ -21,6 +37,9 @@ class InterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        let session = WCSession.default
+        session.delegate = self
+        session.activate()
     }
     
     override func didDeactivate() {
@@ -28,4 +47,17 @@ class InterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
 
+}
+
+//nifty functions to turn doubles into data objects and back
+extension Data {
+    
+    init<T>(from value: T) {
+        var value = value
+        self.init(buffer: UnsafeBufferPointer(start: &value, count: 1))
+    }
+    
+    func to<T>(type:T.Type) -> T {
+        return self.withUnsafeBytes{ $0.pointee}
+    }
 }
--- watch_connectivity/watch_connectivity/ViewController.swift
+++ watch_connectivity/watch_connectivity/ViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import WatchConnectivity
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WCSessionDelegate {
+    
+    // no idea what these are for
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        return
+    }
+    
+    // no idea what these are for
+    func sessionDidBecomeInactive(_ session: WCSession) {
+        return
+    }
+    
+    // no idea what these are for
+    func sessionDidDeactivate(_ session: WCSession) {
+        return
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if (WCSession.isSupported()) {
+            let session = WCSession.default
+            session.delegate = self
+            session.activate()
+            while true {
+                send_a_message()
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +43,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func send_a_message() {
+        let currentTime = CFAbsoluteTimeGetCurrent()
+        
+        if (WCSession.default.isReachable) {
+            let message = Data(from: currentTime)
+            WCSession.default.sendMessageData(message, replyHandler: nil, errorHandler: nil)
+        }
+    }
 
+}
+
+//nifty functions to turn doubles into data objects and back
+extension Data {
+    
+    init<T>(from value: T) {
+        var value = value
+        self.init(buffer: UnsafeBufferPointer(start: &value, count: 1))
+    }
+    
+    func to<T>(type:T.Type) -> T {
+        return self.withUnsafeBytes{ $0.pointee}
+    }
 }
 

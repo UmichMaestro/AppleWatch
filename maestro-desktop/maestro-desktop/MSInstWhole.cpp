ee0ef5613ba1ff5bd8ef07332f5403a21b2b572f
Jake Baur <cjbaur@umich.edu>
2018-11-11 14:41:10 -0500

    updated sound production includes articulation and MSInstWhole

---
+++ maestro-desktop/maestro-desktop/MSInstWhole.cpp
@@ -0,0 +1,9 @@
+//
+//  MSInstWhole.cpp
+//  maestro-desktop
+//
+//  Created by Christopher Baur on 11/3/18.
+//  Copyright © 2018 Christopher Baur. All rights reserved.
+//
+
+#include "MSInstWhole.hpp"
---
+++ maestro-desktop/maestro-desktop/MSInstWhole.h
@@ -0,0 +1,14 @@
+//
+//  MSInstWhole.hpp
+//  maestro-desktop
+//
+//  Created by Christopher Baur on 11/3/18.
+//  Copyright © 2018 Christopher Baur. All rights reserved.
+//
+
+#ifndef MSInstWhole_hpp
+#define MSInstWhole_hpp
+
+#include <stdio.h>
+
+#endif /* MSInstWhole_hpp */
